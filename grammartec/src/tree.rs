nautilus-fuzz/nautilus

commit fcdf89d7d60b37f6d5fc69f7ad458df42aac8579

Updated dependencies, nightly toolchain (#20)
* Updated dependencies, nightly toolchain

* fixed tests for fuzzer

* Updated rand, fixes, clippy fixes

* Clippy pedantic

* Latest rust-toolchain, more clippy

grammartec/src/tree.rs
@@ -52,57 +52,57 @@ impl<'data, 'tree: 'data, 'ctx: 'data, W: Write, T: TreeLike> Unparser<'data, 't
         let nt = tree.get_rule(NodeID::from(i), ctx).nonterm();
         let op = UnparseStep::<'data>::Nonterm(nt);
         let stack = vec![op];
-        return Self {
+        Self {
             stack,
             buffers: vec![],
             w,
             tree,
             i,
             ctx,
-        };
+        }
     }
 
     fn unparse_step(&mut self) -> bool {
         match self.stack.pop() {
             Some(UnparseStep::Term(data)) => self.write(data),
             Some(UnparseStep::Nonterm(nt)) => self.nonterm(nt),
-            Some(UnparseStep::Script(num, expr)) => self.unwrap_script(num, expr),
+            Some(UnparseStep::Script(num, expr)) => self.unwrap_script(num, &expr),
             Some(UnparseStep::PushBuffer()) => self.push_buffer(),
             None => return false,
         };
-        return true;
+        true
     }
 
     fn write(&mut self, data: &[u8]) {
         if let Some(buff) = self.buffers.last_mut() {
-            buff.write(data).unwrap();
+            buff.write_all(data).unwrap();
         } else {
-            self.w.write(data).unwrap();
+            self.w.write_all(data).unwrap();
         }
     }
 
     fn nonterm(&mut self, nt: NTermID) {
         self.next_rule(nt);
     }
-    fn unwrap_script(&mut self, num: usize, expr: PyObject) {
-        let gil = Python::acquire_gil();
-        let py = gil.python();
-        self.script(py, num, expr)
-            .map_err(|e| e.print_and_set_sys_last_vars(py))
-            .unwrap();
+    fn unwrap_script(&mut self, num: usize, expr: &PyObject) {
+        Python::with_gil(|py| {
+            self.script(py, num, expr)
+                .map_err(|e| e.print_and_set_sys_last_vars(py))
+                .unwrap();
+        });
     }
-    fn script(&mut self, py: Python, num: usize, expr: PyObject) -> PyResult<()> {
+    fn script(&mut self, py: Python, num: usize, expr: &PyObject) -> PyResult<()> {
         let bufs = self.buffers.split_off(self.buffers.len() - num);
         let bufs = bufs
             .into_iter()
-            .map(|cur| cur.into_inner())
+            .map(std::io::Cursor::into_inner)
             .collect::<Vec<_>>();
         let byte_arrays = bufs.iter().map(|b| PyBytes::new(py, b));
         let res = expr.call1(py, PyTuple::new(py, byte_arrays))?;
-        if res.as_ref(py).is_instance::<PyString>()? {
+        if res.as_ref(py).is_instance_of::<PyString>()? {
             let pystr = <&PyString>::extract(res.as_ref(py))?;
             self.write(pystr.to_string_lossy().as_bytes());
-        } else if res.as_ref(py).is_instance::<PyBytes>()? {
+        } else if res.as_ref(py).is_instance_of::<PyBytes>()? {
             let pybytes = <&PyBytes>::extract(res.as_ref(py))?;
             self.write(pybytes.as_bytes());
         } else {
@@ -110,7 +110,7 @@ impl<'data, 'tree: 'data, 'ctx: 'data, W: Write, T: TreeLike> Unparser<'data, 't
                 "script function should return string or bytes",
             ));
         }
-        return Ok(());
+        Ok(())
     }
 
     fn push_buffer(&mut self) {
@@ -132,7 +132,7 @@ impl<'data, 'tree: 'data, 'ctx: 'data, W: Write, T: TreeLike> Unparser<'data, 't
     fn next_plain(&mut self, r: &'ctx PlainRule) {
         for rule_child in r.children.iter().rev() {
             let op = match rule_child {
-                RuleChild::Term(data) => UnparseStep::<'data>::Term(&data),
+                RuleChild::Term(data) => UnparseStep::<'data>::Term(data),
                 RuleChild::NTerm(id) => UnparseStep::<'data>::Nonterm(*id),
             };
             self.stack.push(op);
@@ -141,12 +141,12 @@ impl<'data, 'tree: 'data, 'ctx: 'data, W: Write, T: TreeLike> Unparser<'data, 't
 
     fn next_script(&mut self, r: &ScriptRule) {
         {
-            let gil = Python::acquire_gil();
-            let py = gil.python();
-            self.stack.push(UnparseStep::Script(
-                r.nonterms.len(),
-                r.script.clone_ref(py),
-            ));
+            Python::with_gil(|py| {
+                self.stack.push(UnparseStep::Script(
+                    r.nonterms.len(),
+                    r.script.clone_ref(py),
+                ));
+            });
         }
         for nterm in r.nonterms.iter().rev() {
             self.stack.push(UnparseStep::Nonterm(*nterm));
@@ -155,12 +155,12 @@ impl<'data, 'tree: 'data, 'ctx: 'data, W: Write, T: TreeLike> Unparser<'data, 't
     }
 
     fn next_regexp(&mut self, data: &'tree [u8]) {
-        self.stack.push(UnparseStep::<'data>::Term(&data));
+        self.stack.push(UnparseStep::<'data>::Term(data));
     }
 
     fn unparse(&mut self) -> NodeID {
         while self.unparse_step() {}
-        return NodeID::from(self.i);
+        NodeID::from(self.i)
     }
 }
 
@@ -193,7 +193,7 @@ where
     fn unparse_node_to_vec(&self, n: NodeID, ctx: &Context) -> Vec<u8> {
         let mut data = vec![];
         self.unparse(n, ctx, &mut data);
-        return data;
+        data
     }
 
     fn unparse_print(&self, ctx: &Context) {
@@ -214,11 +214,11 @@ impl TreeLike for Tree {
     }
 
     fn size(&self) -> usize {
-        return self.rules.len();
+        self.rules.len()
     }
 
     fn to_tree(&self, _ctx: &Context) -> Tree {
-        return self.clone();
+        self.clone()
     }
 
     fn get_rule<'c>(&self, n: NodeID, ctx: &'c Context) -> &'c Rule {
@@ -233,6 +233,7 @@ impl TreeLike for Tree {
 }
 
 impl Tree {
+    #[must_use]
     pub fn from_rule_vec(rules: Vec<RuleIDOrCustom>, ctx: &Context) -> Self {
         let sizes = vec![0; rules.len()];
         let paren = vec![NodeID::from(0); rules.len()];
@@ -241,20 +242,23 @@ impl Tree {
             sizes,
             paren,
         };
-        if res.rules.len() > 0 {
+        if res.rules.is_empty() {
             res.calc_subtree_sizes_and_parents(ctx);
         }
-        return res;
+        res
     }
 
+    #[must_use]
     pub fn get_rule_id(&self, n: NodeID) -> RuleID {
-        return self.rules[n.to_i()].id();
+        self.rules[n.to_i()].id()
     }
 
+    #[must_use]
     pub fn subtree_size(&self, n: NodeID) -> usize {
-        return self.sizes[n.to_i()];
+        self.sizes[n.to_i()]
     }
 
+    #[must_use]
     pub fn mutate_replace_from_tree<'a>(
         &'a self,
         n: NodeID,
@@ -289,10 +293,10 @@ impl Tree {
             let nonterm = self.get_rule(node_id, ctx).nonterm();
             //sanity check
             let (nterm_id, node) = stack.pop().expect("Not a valid tree for unparsing!");
-            if nterm_id != nonterm {
-                panic!("Not a valid tree for unparsing!");
-            } else {
+            if nterm_id == nonterm {
                 self.paren[i] = node;
+            } else {
+                panic!("Not a valid tree for unparsing!");
             }
             let rule = self.get_rule(node_id, ctx);
             for nonterm in rule.nonterms().iter().rev() {
@@ -303,7 +307,7 @@ impl Tree {
 
     fn calc_sizes(&mut self) {
         //Initiate with 1
-        for size in self.sizes.iter_mut() {
+        for size in &mut self.sizes {
             *size = 1;
         }
         for i in (1..self.size()).rev() {
@@ -312,14 +316,16 @@ impl Tree {
     }
 
     fn slice(&self, from: NodeID, to: NodeID) -> &[RuleIDOrCustom] {
-        return &self.rules[from.into()..to.into()];
+        &self.rules[from.into()..to.into()]
     }
 
+    #[must_use]
     pub fn get_parent(&self, n: NodeID) -> Option<NodeID> {
-        if n != NodeID::from(0) {
-            return Some(self.paren[n.to_i()]);
+        if n == NodeID::from(0) {
+            None
+        } else {
+            Some(self.paren[n.to_i()])
         }
-        return None;
     }
 
     pub fn truncate(&mut self) {
@@ -340,7 +346,7 @@ impl Tree {
                 self.rules.push(RuleIDOrCustom::Rule(ruleid));
                 self.sizes.push(0);
                 self.paren.push(NodeID::from(0));
-                ctx.get_rule(ruleid).generate(self, &ctx, max_len);
+                ctx.get_rule(ruleid).generate(self, ctx, max_len);
                 self.sizes[0] = self.rules.len();
             }
             Rule::RegExp(RegExpRule { hir, .. }) => {
@@ -357,25 +363,27 @@ impl Tree {
         }
     }
 
+    #[must_use]
     pub fn calc_recursions(&self, ctx: &Context) -> Option<Vec<RecursionInfo>> {
         let mut ret = Vec::new();
         let mut done_nterms = HashSet::new();
         for rule in &self.rules {
-            let nterm = ctx.get_nt(&rule);
+            let nterm = ctx.get_nt(rule);
             if !done_nterms.contains(&nterm) {
-                match RecursionInfo::new(self, nterm, ctx) {
-                    Some(rec_info) => ret.push(rec_info),
-                    None => {}
+                if let Some(rec_info) = RecursionInfo::new(self, nterm, ctx) {
+                    ret.push(rec_info);
                 }
                 done_nterms.insert(nterm);
             }
         }
         if ret.is_empty() {
-            return None;
+            None
+        } else {
+            Some(ret)
         }
-        return Some(ret);
     }
 
+    #[must_use]
     pub fn find_recursions_iter(&self, ctx: &Context) -> Vec<(NodeID, NodeID)> {
         let mut found_recursions = Vec::new();
         //Only search for iterations for up to 10000 nodes
@@ -395,7 +403,7 @@ impl Tree {
                 depth += 1;
             }
         }
-        return found_recursions;
+        found_recursions
     }
 }
 
@@ -406,6 +414,7 @@ pub struct TreeMutation<'a> {
 }
 
 impl<'a> TreeMutation<'a> {
+    #[must_use]
     pub fn get_at(&self, n: NodeID) -> &'a RuleIDOrCustom {
         let i = n.to_i();
         let end0 = self.prefix.len();
@@ -426,11 +435,11 @@ impl<'a> TreeMutation<'a> {
 
 impl<'a> TreeLike for TreeMutation<'a> {
     fn get_rule_id(&self, n: NodeID) -> RuleID {
-        return self.get_at(n).id();
+        self.get_at(n).id()
     }
 
     fn size(&self) -> usize {
-        return self.prefix.len() + self.repl.len() + self.postfix.len();
+        self.prefix.len() + self.repl.len() + self.postfix.len()
     }
     fn get_rule_or_custom(&self, n: NodeID) -> &RuleIDOrCustom {
         self.get_at(n)
@@ -438,10 +447,10 @@ impl<'a> TreeLike for TreeMutation<'a> {
 
     fn to_tree(&self, ctx: &Context) -> Tree {
         let mut vec = vec![];
-        vec.extend_from_slice(&self.prefix);
-        vec.extend_from_slice(&self.repl);
-        vec.extend_from_slice(&self.postfix);
-        return Tree::from_rule_vec(vec, ctx);
+        vec.extend_from_slice(self.prefix);
+        vec.extend_from_slice(self.repl);
+        vec.extend_from_slice(self.postfix);
+        Tree::from_rule_vec(vec, ctx)
     }
 
     fn get_rule<'c>(&self, n: NodeID, ctx: &'c Context) -> &'c Rule {
@@ -468,7 +477,7 @@ mod tests {
             size += sub_size;
         }
         tree.sizes[n.to_i()] = size;
-        return size;
+        size
     }
 
     #[test]

forksrv/Cargo.toml
@@ -14,7 +14,7 @@ lazy_static = "1.4.0"
 libc = "0.2.70"
 quick-error = "1.2.3"
 tempfile = "3.1.0"
-rand = "0.7.3"
+rand = "0.8"
 serde_derive = "1.0"
 serde = "1.0"
 byteorder = "1.3.4"

forksrv/src/exitreason.rs
@@ -25,12 +25,13 @@ pub enum ExitReason {
 }
 
 impl ExitReason {
+    #[must_use]
     pub fn from_wait_status(status: WaitStatus) -> ExitReason {
-        return match status {
+        match status {
             WaitStatus::Exited(_, return_value) => ExitReason::Normal(return_value),
             WaitStatus::Signaled(_, signal, _) => ExitReason::Signaled(signal as i32),
             WaitStatus::Stopped(_, signal) => ExitReason::Stopped(signal as i32),
-            _ => panic!("Unknown WaitStatus: {:?}", status),
-        };
+            _ => panic!("{}", "Unknown WaitStatus: {status:?}"),
+        }
     }
 }

forksrv/src/lib.rs
@@ -25,10 +25,9 @@ extern crate serde_derive;
 pub mod exitreason;
 pub mod newtypes;
 
+use nix::errno::errno;
 use nix::fcntl;
-use nix::libc::{
-    __errno_location, shmat, shmctl, shmget, strerror, IPC_CREAT, IPC_EXCL, IPC_PRIVATE, IPC_RMID,
-};
+use nix::libc::{shmat, shmctl, shmget, strerror, IPC_CREAT, IPC_EXCL, IPC_PRIVATE, IPC_RMID};
 use nix::sys::signal::{self, Signal};
 use nix::sys::stat;
 use nix::sys::wait::WaitStatus;
@@ -49,7 +48,7 @@ use std::fs::File;
 use std::os::unix::io::FromRawFd;
 
 use exitreason::ExitReason;
-use newtypes::*;
+use newtypes::{QemuRunIO, QemuRunNix, SubprocessError};
 use snafu::ResultExt;
 
 pub struct ForkServer {
@@ -60,6 +59,7 @@ pub struct ForkServer {
 }
 
 impl ForkServer {
+    #[must_use]
     pub fn new(
         path: String,
         args: Vec<String>,
@@ -96,12 +96,12 @@ impl ForkServer {
                 st_out
                     .read_u32::<LittleEndian>()
                     .expect("couldn't read child hello");
-                return Self {
-                    inp_file: inp_file,
+                Self {
+                    inp_file,
                     ctl_in: unsafe { File::from_raw_fd(ctl_in) },
-                    shared_data: shared_data,
+                    shared_data,
                     st_out,
-                };
+                }
             }
             //Child does complex stuff
             ForkResult::Child => {
@@ -125,7 +125,7 @@ impl ForkServer {
                     .map(|s| CString::new(s).expect("args must not contain zero"))
                     .collect::<Vec<_>>();
 
-                let shm_id = CString::new(format!("__AFL_SHM_ID={}", shm_file)).unwrap();
+                let shm_id = CString::new(format!("__AFL_SHM_ID={shm_file}")).unwrap();
 
                 //Asan options: set asan SIG to 223 and disable leak detection
                 let asan_settings =
@@ -141,7 +141,7 @@ impl ForkServer {
                     unistd::dup2(null, 2 as RawFd).expect("couldn't dup2 /dev/null to stderr");
                     unistd::close(null).expect("couldn't close /dev/null");
                 }
-                println!("EXECVE {:?} {:?} {:?}", path, args, env);
+                println!("EXECVE {path:?} {args:?} {env:?}");
                 unistd::execve(&path, &args, &env).expect("couldn't execve afl-qemu-tarce");
                 unreachable!();
             }
@@ -188,49 +188,51 @@ impl ForkServer {
         self.st_out.read_u32::<LittleEndian>().context(QemuRunIO {
             task: "couldn't read timeout exitcode",
         })?;
-        return Ok(ExitReason::Timeouted);
+        Ok(ExitReason::Timeouted)
     }
 
     pub fn get_shared_mut(&mut self) -> &mut [u8] {
-        unsafe { return &mut *self.shared_data }
+        unsafe { &mut *self.shared_data }
     }
+    #[must_use]
     pub fn get_shared(&self) -> &[u8] {
-        unsafe { return &*self.shared_data }
+        unsafe { &*self.shared_data }
     }
 
     fn create_shm(bitmap_size: usize) -> (i32, *mut [u8]) {
         unsafe {
             let shm_id = shmget(IPC_PRIVATE, bitmap_size, IPC_CREAT | IPC_EXCL | 0o600);
-            if shm_id < 0 {
-                panic!(
-                    "shm_id {:?}",
-                    CString::from_raw(strerror(*__errno_location()))
-                );
-            }
+            assert!(
+                shm_id >= 0,
+                "shm_id {:?}",
+                CString::from_raw(strerror(errno()))
+            );
 
             let trace_bits = shmat(shm_id, ptr::null(), 0);
-            if (trace_bits as isize) < 0 {
-                panic!(
-                    "shmat {:?}",
-                    CString::from_raw(strerror(*__errno_location()))
-                );
-            }
+            assert!(
+                (trace_bits as isize) >= 0,
+                "shmat {:?}",
+                CString::from_raw(strerror(errno()))
+            );
 
-            let res = shmctl(shm_id, IPC_RMID, 0 as *mut nix::libc::shmid_ds);
-            if res < 0 {
-                panic!(
-                    "shmclt {:?}",
-                    CString::from_raw(strerror(*__errno_location()))
-                );
-            }
-            return (shm_id, trace_bits as *mut [u8; 1 << 16]);
+            let res = shmctl(
+                shm_id,
+                IPC_RMID,
+                std::ptr::null_mut::<nix::libc::shmid_ds>(),
+            );
+            assert!(
+                res >= 0,
+                "shmclt {:?}",
+                CString::from_raw(strerror(errno()))
+            );
+            (shm_id, trace_bits.cast::<[u8; 65536]>())
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::*;
+    use crate::{exitreason, ForkServer};
     #[test]
     fn run_forkserver() {
         let hide_output = false;

fuzzer/Cargo.toml
@@ -18,8 +18,8 @@ serde_json = "1.0"
 serde_derive = "1.0"
 argparse = "0.2.2"
 ron = "*"
-clap = "2.33.1"
-pyo3 = "0.15.0"
+clap = "3.2"
+pyo3 = "0.17"
 
 [[bin]]
 name = "fuzzer"

fuzzer/src/fuzzer.rs
@@ -33,6 +33,7 @@ use grammartec::context::Context;
 use grammartec::tree::TreeLike;
 use shared_state::GlobalSharedState;
 
+#[derive(Debug, Clone, Copy)]
 pub enum ExecutionReason {
     Havoc,
     HavocRec,
@@ -80,7 +81,7 @@ impl Fuzzer {
         hide_output: bool,
         timeout_in_millis: u64,
         bitmap_size: usize,
-    ) -> Result<Self, SubprocessError> {
+    ) -> Self {
         let fs = ForkServer::new(
             path.clone(),
             args.clone(),
@@ -88,7 +89,7 @@ impl Fuzzer {
             timeout_in_millis,
             bitmap_size,
         );
-        return Ok(Fuzzer {
+        Fuzzer {
             forksrv: fs,
             last_tried_inputs: HashSet::new(),
             last_inputs_ring_buffer: VecDeque::new(),
@@ -113,8 +114,8 @@ impl Fuzzer {
             asan_found_by_det: 0,
             asan_found_by_det_afl: 0,
             asan_found_by_gen: 0,
-            work_dir: work_dir,
-        });
+            work_dir,
+        }
     }
 
     pub fn run_on_with_dedup<T: TreeLike>(
@@ -128,7 +129,7 @@ impl Fuzzer {
             return Ok(false);
         }
         self.run_on(&code, tree, exec_reason, ctx)?;
-        return Ok(true);
+        Ok(true)
     }
 
     pub fn run_on_without_dedup<T: TreeLike>(
@@ -138,12 +139,12 @@ impl Fuzzer {
         ctx: &Context,
     ) -> Result<(), SubprocessError> {
         let code = tree.unparse_to_vec(ctx);
-        return self.run_on(&code, tree, exec_reason, ctx);
+        self.run_on(&code, tree, exec_reason, ctx)
     }
 
     fn run_on<T: TreeLike>(
         &mut self,
-        code: &Vec<u8>,
+        code: &[u8],
         tree: &T,
         exec_reason: ExecutionReason,
         ctx: &Context,
@@ -224,8 +225,8 @@ impl Fuzzer {
                         .last_found_sig =
                         strftime("[%Y-%m-%d] %H:%M:%S", &othertime::now()).expect("RAND_76391000");
                     let mut file = File::create(format!(
-                        "{}/outputs/signaled/{:?}_{:09}",
-                        self.work_dir, sig, self.execution_count
+                        "{}/outputs/signaled/{sig:?}_{:09}",
+                        self.work_dir, self.execution_count
                     ))
                     .expect("RAND_3690294970");
                     tree.unparse_to(ctx, &mut file);
@@ -234,7 +235,7 @@ impl Fuzzer {
             ExitReason::Stopped(_sig) => {}
         }
         stdout().flush().expect("RAND_2937475131");
-        return Ok(());
+        Ok(())
     }
 
     pub fn has_bits<T: TreeLike>(
@@ -253,27 +254,27 @@ impl Fuzzer {
                 found_all = false;
             }
         }
-        return Ok(found_all);
+        Ok(found_all)
     }
 
-    pub fn exec_raw<'a>(&'a mut self, code: &[u8]) -> Result<(ExitReason, u32), SubprocessError> {
+    pub fn exec_raw(&mut self, code: &[u8]) -> Result<(ExitReason, u32), SubprocessError> {
         self.execution_count += 1;
 
         let start = Instant::now();
 
-        let exitreason = self.forksrv.run(&code)?;
+        let exitreason = self.forksrv.run(code)?;
 
         let execution_time = start.elapsed().subsec_nanos();
 
         self.average_executions_per_sec = self.average_executions_per_sec * 0.9
-            + ((1.0 / (execution_time as f32)) * 1000000000.0) * 0.1;
+            + ((1.0 / (execution_time as f32)) * 1_000_000_000.0) * 0.1;
 
-        return Ok((exitreason, execution_time));
+        Ok((exitreason, execution_time))
     }
 
     fn input_is_known(&mut self, code: &[u8]) -> bool {
         if self.last_tried_inputs.contains(code) {
-            return true;
+            true
         } else {
             self.last_tried_inputs.insert(code.to_vec());
             if self.last_inputs_ring_buffer.len() == 10000 {
@@ -285,8 +286,8 @@ impl Fuzzer {
                 );
             }
             self.last_inputs_ring_buffer.push_front(code.to_vec());
+            false
         }
-        return false;
     }
 
     fn exec<T: TreeLike>(
@@ -295,13 +296,12 @@ impl Fuzzer {
         tree_like: &T,
         ctx: &Context,
     ) -> Result<(Option<Vec<usize>>, ExitReason), SubprocessError> {
-        let (exitreason, execution_time) = self.exec_raw(&code)?;
+        let (exitreason, execution_time) = self.exec_raw(code)?;
 
-        let is_crash = match exitreason {
-            ExitReason::Normal(223) => true,
-            ExitReason::Signaled(_) => true,
-            _ => false,
-        };
+        let is_crash = matches!(
+            exitreason,
+            ExitReason::Normal(223) | ExitReason::Signaled(_)
+        );
 
         let mut final_bits = None;
         if let Some(mut new_bits) = self.new_bits(is_crash) {
@@ -309,8 +309,8 @@ impl Fuzzer {
             if exitreason != ExitReason::Timeouted {
                 //Check for non deterministic bits
                 let old_bitmap: Vec<u8> = self.forksrv.get_shared().to_vec();
-                self.check_deterministic_behaviour(&old_bitmap, &mut new_bits, &code)?;
-                if new_bits.len() > 0 {
+                self.check_deterministic_behaviour(&old_bitmap, &mut new_bits, code)?;
+                if !new_bits.is_empty() {
                     final_bits = Some(new_bits);
                     let tree = tree_like.to_tree(ctx);
                     self.global_state
@@ -322,7 +322,7 @@ impl Fuzzer {
                 }
             }
         }
-        return Ok((final_bits, exitreason));
+        Ok((final_bits, exitreason))
     }
 
     fn check_deterministic_behaviour(
@@ -336,12 +336,12 @@ impl Fuzzer {
             let run_bitmap = self.forksrv.get_shared();
             for (i, &v) in old_bitmap.iter().enumerate() {
                 if run_bitmap[i] != v {
-                    println!("found fucky bit {}", i);
+                    println!("found fucky bit {i}");
                 }
             }
             new_bits.retain(|&i| run_bitmap[i] != 0);
         }
-        return Ok(());
+        Ok(())
     }
 
     pub fn new_bits(&mut self, is_crash: bool) -> Option<Vec<usize>> {
@@ -361,10 +361,10 @@ impl Fuzzer {
             }
         }
 
-        if res.len() > 0 {
+        if !res.is_empty() {
             //print!("New path found:\nNew bits: {:?}\n", res);
             return Some(res);
         }
-        return None;
+        None
     }
 }

fuzzer/src/generator.rs
@@ -36,27 +36,27 @@ fn main() {
     let matches = App::new("generator")
         .about("Generate strings using a grammar. This can also be used to generate a corpus")
         .arg(Arg::with_name("grammar_path")
-             .short("g")
+             .short('g')
              .value_name("GRAMMAR")
              .takes_value(true)
              .required(true)
              .help("Path to grammar"))
         .arg(Arg::with_name("tree_depth")
-             .short("t")
+             .short('t')
              .value_name("DEPTH")
              .takes_value(true)
              .required(true)
              .help("Size of trees that are generated"))
         .arg(Arg::with_name("number_of_trees")
-             .short("n")
+             .short('n')
              .value_name("NUMBER")
              .takes_value(true)
              .help("Number of trees to generate [default: 1]"))
         .arg(Arg::with_name("store")
-             .short("s")
+             .short('s')
              .help("Store output to files. This will create a folder called corpus containing one file for each generated tree."))
         .arg(Arg::with_name("verbose")
-             .short("v")
+             .short('v')
              .help("Be verbose"))
         .get_matches();
 
@@ -76,7 +76,7 @@ fn main() {
         let gf = File::open(grammar_path).expect("cannot read grammar file");
         let rules: Vec<Vec<String>> =
             serde_json::from_reader(&gf).expect("cannot parse grammar file");
-        assert!(rules.len() > 0, "rule file didn_t include any rules");
+        assert!(!rules.is_empty(), "rule file didn_t include any rules");
         let root = "{".to_string() + &rules[0][0] + "}";
         ctx.add_rule("START", root.as_bytes());
         for rule in rules {
@@ -102,7 +102,7 @@ fn main() {
         let len = ctx.get_random_len_for_nt(&nonterm);
         let generated_tree = ctx.generate_tree_from_nt(nonterm, len); //1 is the index of the "START" Node
         if verbose {
-            println!("Generating tree {} from {}", i + 1, number_of_trees);
+            println!("Generating tree {} from {number_of_trees}", i + 1);
         }
         if store {
             let mut output =
@@ -114,7 +114,7 @@ fn main() {
             generated_tree.unparse_to(&ctx, &mut stdout_handle);
         }
 
-        let mut of_tree = File::create(&"/tmp/test_tree.ron").expect("cannot create output file");
+        let mut of_tree = File::create("/tmp/test_tree.ron").expect("cannot create output file");
         of_tree
             .write_all(
                 ron::ser::to_string(&generated_tree)

commit 0b5239674686c1f2cd9be845e690781450a04c97

fix bug in tree.rs
this bug was introduced in fcdf89d7d60b37f6d5fc69f7ad458df42aac8579
and is causing libafl based fuzzers using nautilus to crash.

grammartec/src/tree.rs
@@ -242,7 +242,7 @@ impl Tree {
             sizes,
             paren,
         };
-        if res.rules.is_empty() {
+        if !res.rules.is_empty() {
             res.calc_subtree_sizes_and_parents(ctx);
         }
         res