cdb0661e8260d6615736d06e46a4ccb9a5f268ca
R. Elliott Childre <elliottchildre329@gmail.com>
2023-01-10 09:55:26 -0500

    Move Nautilus workspace members to Rust Stable
    Nautilus is one of the main features keeping LibAFL on the nightly
    channel.
    
    The features of `exclusive_range_pattern` and `step_trait` are both
    nonessentially used in the code base and seem to not be being merged
    into rust stable anytime soon.
    
    By trivially removing the test the repo compiles with Rust stable with
    an MSRV of 1.58 (Released 2022-01-13).
    
    I tested LibAFL with the "nautilus" feature pointed to this commit and
    the feature remains unchanged.

--- grammartec/src/lib.rs
+++ grammartec/src/lib.rs
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#![feature(exclusive_range_pattern)]
-#![feature(step_trait)]
 #[macro_use]
 extern crate lazy_static;
 #[macro_use]
